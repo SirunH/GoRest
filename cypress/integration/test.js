d23ac680a0ad49ce551a9854bd9ba1460b5ee8f9
Gevorg Adamyan <adamyangevorg4@gmail.com>
2022-07-29 18:14:50 +0400

    Add new requests

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -3,14 +3,19 @@
 import requests from '../support/requests/requests'
 
 describe('API testing on GoRest.io', () => {
-    it('API testing on Users ', () => {
+    before(() => {
+        // requests.createDataBase();
+        // requests.getAllUsersPerPageAndAddInfoToDatabase(); //Don't run this code, it will take 20 minutes!!!!
         requests.getAllUsersAndCheckInfo();
+    })
+
+    it('API testing on Users ', () => {
         requests.getRandomUserAndCheckInfo();
-        requests.createUserAndCheckInfo();
-        requests.getCreatedUserAndChekInfo();
-        requests.editUserInfoWithPatchAndCheckInfo();
-        requests.editUserWithPutAndCheckInfo();
-        requests.deleteRandomUserAndCheckResults();
-        requests.getTooManyRequestsMessage();
+        // requests.createUserAndCheckInfo();
+        // requests.getCreatedUserAndChekInfo();
+        // requests.editRandomUserInfoWithPatchAndCheckInfo();
+        // requests.editRandomUserWithPutAndCheckInfo()
+        // requests.deleteUserInfoFromDataBase(4625);
+        // requests.getTooManyRequestsMessage();
     });
 })
--- cypress/support/requests/requests.js
+++ cypress/support/requests/requests.js
@@ -16,19 +16,40 @@ class Requests {
         })
     }
 
-    getAllUsersAndCheckInfo() {
-        this.getAllUsers()
-            .then(res => {
-                cy.log(res)
-                expect(res.body.code).to.eq(200)
-                res.body.data.forEach(el => {
-                    expect(el.id).not.to.be.null;
-                })
-                let usersNumber = res.body.data.length;
-                let randomIndex = Math.floor(Math.random() * usersNumber);
-                let randomUserId = res.body.data[randomIndex].id;
-                cy.wrap(randomUserId).as('randomId')
+    getAllUsersPerPageAndAddInfoToDatabase() {
+        this.getAllUsers().then(res => {
+            let i;
+            for (i = 1; i <= res.body.meta.pagination.pages; i++) {
+                this.getAllUsersPerPage(i)
+            }
+        })
+    }
+
+    getAllUsersPerPage(page) {
+        cy.request({
+            method: 'GET',
+            url: `//users?page=${page}`,
+            auth: {
+                bearer: data.token
+            }
+        }).then(res => {
+            res.body.data.forEach(el => {
+                this.addUserInfoToDataBase(el.id, el.name, el.email, el.gender, el.status)
             })
+        })
+    }
+
+    getAllUsersAndCheckInfo() {
+        this.getAllUsers().then(res => {
+            expect(res.body.code).to.eq(200)
+            res.body.data.forEach(el => {
+                expect(el.id).not.to.be.null;
+            })
+            let usersNumber = res.body.data.length;
+            let randomIndex = Math.floor(Math.random() * usersNumber);
+            let randomUserId = res.body.data[randomIndex].id;
+            cy.wrap(randomUserId).as('randomId');
+        })
     }
 
     getUser(id) {
@@ -38,12 +59,17 @@ class Requests {
             auth: {
                 bearer: data.token
             },
-            // failOnStatusCode: false
         })
     }
 
     getUserAndCheckInfo(id) {
         this.getUser(id).then(res => {
+            this.getUserInfoFromDataBase(id).then(info => {
+                expect(info[0].UserName).to.eq(res.body.data.name);
+                expect(info[0].Email).to.eq(res.body.data.email);
+                expect(info[0].Gender).to.eq(res.body.data.gender);
+                expect(info[0].Status).to.eq(res.body.data.status)
+            })
             expect(res.body.data.id).to.eq(id);
             expect(res.body.code).to.eq(200)
         })
@@ -75,6 +101,7 @@ class Requests {
             expect(res.body.data.email).to.eq(data.userEmail);
             expect(res.body.data.status).to.eq(data.userStatus);
             cy.wrap(res.body.data.id).as('createdUserId');
+            this.addUserInfoToDataBase(res.body.data.id, res.body.data.name, res.body.data.email, res.body.data.gender, res.body.data.status)
         })
     }
 
@@ -183,45 +210,55 @@ class Requests {
         })
     }
 
-    editUserInfoWithPatchAndCheckInfo() {
+    editUserInfoWithPatchAndCheckInfo(id) {
+        let controller = Math.round(Math.random() * 3);
+        if (controller === 0)
+            this.editUserEmailAndCheckInfo(id);
+        else if (controller === 1)
+            this.editUserNameAndCheckInfo(id);
+        else if (controller === 2)
+            this.editUserStatusAndCheckInfo(id);
+        else
+            this.editUserGenderAndCheckInfo(id);
+        this.updateUserInfoInDataBase(id);
+    }
+
+    editRandomUserInfoWithPatchAndCheckInfo() {
         cy.get('@randomId').then(id => {
-            let controller = Math.round(Math.random() * 3);
-            if (controller === 0)
-                this.editUserEmailAndCheckInfo(id);
-            else if (controller === 1)
-                this.editUserNameAndCheckInfo(id);
-            else if (controller === 2)
-                this.editUserStatusAndCheckInfo(id);
-            else
-                this.editUserGenderAndCheckInfo(id)
+            this.editUserInfoWithPatchAndCheckInfo(id);
         })
     }
 
-    editUserWithPutAndCheckInfo() {
+    editUserWithPutAndCheckInfo(id) {
         let newName = faker.internet.userName();
         let newEmail = faker.internet.email();
         let newGender = helpers.getRandomGender();
         let newStatus = helpers.getRandomStatus();
+        cy.request({
+            method: 'PATCH',
+            url: `//users/${id}`,
+            auth: {
+                bearer: data.token
+            },
+            body: {
+                "name": newName,
+                "gender": newGender,
+                "email": newEmail,
+                "status": newStatus
+            }
+        }).then(res => {
+            expect(res.body.code).to.eq(200);
+            expect(res.body.data.name).to.eq(newName);
+            expect(res.body.data.gender).to.eq(newGender);
+            expect(res.body.data.email).to.eq(newEmail);
+            expect(res.body.data.status).to.eq(newStatus);
+        })
+        this.updateUserInfoInDataBase(id)
+    }
+
+    editRandomUserWithPutAndCheckInfo() {
         cy.get('@randomId').then(id => {
-            cy.request({
-                method: 'PATCH',
-                url: `//users/${id}`,
-                auth: {
-                    bearer: data.token
-                },
-                body: {
-                    "name": newName,
-                    "gender": newGender,
-                    "email": newEmail,
-                    "status": newStatus
-                }
-            }).then(res => {
-                expect(res.body.code).to.eq(200);
-                expect(res.body.data.name).to.eq(newName);
-                expect(res.body.data.gender).to.eq(newGender);
-                expect(res.body.data.email).to.eq(newEmail);
-                expect(res.body.data.status).to.eq(newStatus);
-            })
+            this.editUserWithPutAndCheckInfo(id);
         })
     }
 
@@ -234,69 +271,59 @@ class Requests {
             }
         }).its('body.code').should('eq', 204)
         this.getUser(id).its("body.code").should("eq", 404)
+        this.deleteUserInfoFromDataBase(id);
     }
 
     deleteRandomUserAndCheckResults() {
         cy.get('@randomId').then(id => {
-            this.deleteUserAndCheckInfo(id);
-        })
-    }
-
-    getUserPosts(id) {
-        cy.request({
-            method: 'GET',
-            url: `//users/${id}/posts`,
-            auth: {
-                bearer: data.token
-            }
-        }).then(res => {
-            cy.log(res)
-        })
-    }
-
-    getAllPosts() {
-        cy.request({
-            method: 'GET',
-            url: `//posts`,
-            auth: {
-                bearer: data.token
-            }
-        }).then(res => {
-            cy.log(res)
-        })
-    }
-
-    addPost(id) {
-        cy.request({
-            method: 'Post',
-            url: `//users/${id}/posts`,
-            auth: {
-                bearer: data.token
-            },
-            body: {
-                "user": id,
-                "title": data.userPostTitle,
-                "body": data.userPostContent
-            }
-        }).then(res => {
-            cy.log(res)
-        })
-    }
-
-    addUserPost() {
-        cy.get('@randomId').then(id => {
-            this.addPost(id)
+            this.deleteUserAndCheckInfo
+            (id);
         })
     }
 
     getTooManyRequestsMessage() {
-        let i;
-        for(i = 0; i < 1000; i++) {
-            this.getAllUsers().its('body.code').should('not.eq', 429)
-        }
+        this.getAllUsers().then(res => {
+            if (res.body.code !== 429) {
+                this.getTooManyRequestsMessage();
+            } else {
+                cy.log("429: Too many request!!!")
+                expect(res.body.code).to.eq(429)
+            }
+        })
+    }
+
+    createDataBase() {
+        cy.task('queryDb', "CREATE TABLE UserInfo (PersonID int NOT NULL , UserName varchar(255) NOT NULL , Email varchar(255) NOT NULL , Gender varchar(20),Status varchar(20))")
+
     }
 
 
+    addUserInfoToDataBase(id, name, email, gender, status) {
+        cy.task('queryDb', `INSERT INTO UserInfo (PersonID, UserName, Email, Gender, Status) VALUES (${id}, "${name}", "${email}", "${gender}", "${status}");`)
+    }
+
+    addAllUsersInfoToDataBase() {
+        this.getAllUsers().then(res => {
+            res.body.data.forEach(el => {
+                this.addUserInfoToDataBase(el.id, el.name, el.email, el.gender, el.status)
+            })
+        })
+    }
+
+    deleteUserInfoFromDataBase(id) {
+        cy.task('queryDb', `DELETE FROM UserInfo WHERE PersonID=${id};`)
+    }
+
+    updateUserInfoInDataBase(id) {
+        this.deleteUserInfoFromDataBase(id);
+        this.getUser(id).then(res => {
+            this.addUserInfoToDataBase(res.body.data.id, res.body.data.name, res.body.data.email, res.body.data.gender, res.body.data.status)
+        })
+    }
+
+    getUserInfoFromDataBase(id) {
+       return cy.task('queryDb', `SELECT * FROM UserInfo WHERE PersonID=${id};`)
+    }
 }
 
 module.exports = new Requests();
